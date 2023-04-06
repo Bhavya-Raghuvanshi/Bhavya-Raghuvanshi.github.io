3d14ea7f9057f3530e22fcac280f30ac05bf8f67
Bhavya-Raghuvanshi <123796228+Bhavya-Raghuvanshi@users.noreply.github.com>
2023-04-06 14:32:34 +0530

    Add files via upload

---
+++ Feane/Assetes/js/script.js
@@ -0,0 +1,7 @@
+function change(e) {
+    document.querySelector('.submenu > li > button').style.color = "#212832"
+    document.querySelector('.submenu > li > button').style.backgroundColor = "white"
+
+    e.style.color = "white"
+    e.style.backgroundColor = "#212832"
+}

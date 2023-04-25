c6208a1012bf9d27f9071f0e1082b25bebb878ea
Bhavya-Raghuvanshi <123796228+Bhavya-Raghuvanshi@users.noreply.github.com>
2023-04-26 00:03:40 +0530

    Add files via upload

---
+++ Watches/assets/js/script.js
@@ -0,0 +1,10 @@
+function navbtn() {
+
+    if(document.getElementById("m-nav-menu").style.display == "block")
+    {
+        document.getElementById("m-nav-menu").style.display = "none";
+    }
+    else{
+        document.getElementById("m-nav-menu").style.display = "block";
+    }
+}
